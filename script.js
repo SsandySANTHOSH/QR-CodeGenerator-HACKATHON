57dc30fc87dcd7df7461ac997f4c200801016268
Mohan51502 <rajmohan51502@gmail.com>
2023-09-09 22:15:50 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const generateButton = document.getElementById("generate");
+    const downloadLink = document.getElementById("download");
+    const qrCodeContainer = document.getElementById("qr-code");
+
+    generateButton.addEventListener("click", () => {
+        const data = document.getElementById("data").value;
+        const width = document.getElementById("width").value;
+        const height = document.getElementById("height").value;
+        const format = document.getElementById("format").value;
+        const qrColor = document.getElementById("qr-color").value;
+        const bgColor = document.getElementById("bg-color").value;
+
+        const qrCode = `https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(
+            data
+        )}&size=${width}x${height}&format=${format}&color=${qrColor}&bgcolor=${bgColor}`;
+
+        qrCodeContainer.innerHTML = `<img src="${qrCode}" alt="QR Code" width="${width}" height="${height}">`;
+        downloadLink.href = qrCode;
+        downloadLink.style.display = "block";
+    });
+});
